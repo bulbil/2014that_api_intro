48c0a91af198647208d95f8f56d3a8635396bbfe
Nabil Kashyap <n@nabilk.com>
2014-09-17 18:20:30 -0400

    commit before revert

--- js/scripts.js
+++ js/scripts.js
@@ -7,76 +7,94 @@
 // this is a simple script to show off the basics of the DPLA
 // API -- not particularly robust but I hope gives the basic idea
 
-// first get your api key from http://dp.la/info/developers/codex/policies/#get-a-key
-//var apiKey = '[YOUR_API_KEY]',
-var baseURL = 'http://api.dp.la/v2/items?q=',
-// pagination does not start on 0 but on an index of 1
-page = 1;
-
-// bootstrap tooltip instantiation
-$('.tooltip').tooltip();
-
-// search button event
-$('#search').submit(function(e) {
-
-	e.preventDefault();
-
-    page = ($('#thumbs ul li').length == 0) ? 1 : page + 1; 
-    $('#thumbs ul li').remove();
-
-	var input = $('#search input[type=text]').val();
-
-	dplaThumbs.search(input, page);
-
-});
-
 var dplaThumbs = {
+	baseURL: 'http://api.dp.la/v2/items?q=',
+	data: {
+		'q': '',
+		'page': 2,
+		'page_size': 0,
+		'api_key': apiKey
+	},
 
-	search: function(str, int){
-		
+	template: _.template( $('#thumbs_template').html() ),
+	
+	search: function(str, page){
 	// the main object to store your GET parameters ... 
 	// the possibilities are (almost) endless
-		data = { 	
-			'q': str, 
-    		'api_key': apiKey,
-    		'page_size': 10,
-    		'page': int
-	        	};
+		this.data.q = str;
+		this.data.page = page;
+
+	    this.getData()
+	    	.done(_.bind(function(d){ 
+	    		this.showThumbs(d);
+				$('.tooltip').tooltip({
+					animation: true,
+					html: true,
+					placement: 'auto bottom'
+					});
+				}, this)
+			);
+	},
+
+	getData: function(){
 	// the basic ajax request
-		$.ajax({
+		return $.ajax({
 
 	        type: 'GET',
-	        url: baseURL,
-	        data: data,
+	        url: this.baseURL,
+	        data: this.data,
 	        // don't forget! otherwise won't work
 	        dataType: 'jsonp',
-	        success: function(data) {
-			
-			$('#thumbs ul').empty();
-			$('#results h3').show();
-
-		// each request has some overall metadata, like data.count
-		var count = (data.count > 0) ? data.count + ' results' : 'no results';
-		$('#count h1').text(count);
-		console.log(data.docs);
-		// each record is returned in data.docs
-		$.each(data.docs, function(i,d) {
-
-			// d.object, for instance, has the URL to the thumb (low-res preview of the digital object)
-			// for the purpose of this thing, skips a result if no thumbnail
-			if(d.object) {
-				$('#thumbs ul').append('<li>');
-				var $thumb = $('#thumbs li').last();
-				$thumb.append('<a><img>');
-				// d.isShownAt has the URL to the original representation of the record
-				$thumb.children('a').attr( {'href': d.isShownAt, 'target': 'blank'});	
-				$thumb.find('a img').attr('src', d.object);
-				$thumb.tooltip({'title': d.sourceResource.title, 'placement': 'auto bottom'});
-				}
-			});
-		},
-
 	        error: function(e) { console.log(e.message); }
 	    });
+	},
+
+	showThumbs: function(json){
+		// $thumbs = $('#thumbs ul');
+		var $thumbs = $('#thumbs');
+		$thumbs.masonry('remove', $('.thumb'));
+		$results = $('#results h3');
+		$results.find('#page-size').html( this.data['page_size'] );
+		$results.show();
+
+		// each request has some overall metadata, like data.count
+		var count = (json.count > 0) ? json.count + ' results' : 'no results';
+		$('#count h1').text(count);
+		
+		var thumbsArray = [];
+		for(var i =0; i < json.limit; i++ ){
+
+			if(json.docs[i].object){ $thumbs.append($(this.template({thumb: json.docs[i]}))); }
+		}
+		$thumbs.masonry('appended', $('.thumb'));
+		console.log(thumbsArray);
 	}
 }
+
+$(function(){
+
+	$('body').tooltip({
+		selector: '[rel=tooltip]',
+		html: true
+	});
+
+	var $thumbs = $('#thumbs');
+	$('#thumbs').masonry({
+		// options
+		columnWidth: 200,
+		itemSelector: '.thumb',
+		containerStyle: null,
+		// isFitWidth: true,
+		gutter: 15
+	});
+
+	// search button event
+	$('#search').submit(function(e) {
+
+		e.preventDefault();
+	    var page = ($('#thumbs div').length == 0) ? 1 : page + 1; 
+		var input = $('#search input[type=text]').val();
+		dplaThumbs.search(input, page);
+	});
+
+});
